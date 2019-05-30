de43b88680d28898c9064d46b814f36ddf358e99
altrigoBeni <47933427+altrigoBeni@users.noreply.github.com>
2019-05-30 12:06:08 +0200

    Initial commit

---
+++ src/com/cice/PlantillaHtml/Login.java
@@ -0,0 +1,40 @@
+package com.cice.PlantillaHtml;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.coyote.Request;
+
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    } 
+    
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/main.jsp");
+		dispatcher.forward(req, resp);
+	}
+
+
+
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
